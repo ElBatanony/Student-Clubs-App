28384541328365b6fce72f7e5cfb2a51ad2edb96
ElBatanony <ahmedelbatanony@hotmail.com>
2020-07-17 17:21:43 +0300

    Add irregular clubs to summary
    Format document.
    Remove commented code.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,21 +2,14 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp();
 const db = admin.firestore();
-const days : string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const days: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 export const updateClub = functions.firestore
     .document('clubs/{clubId}')
     .onWrite((change, context) => {
 
         // The id of the changed club
-        const clubId : string = context.params.clubId
+        const clubId: string = context.params.clubId
         // The club data (name, location, days, ...)
         const club = change.after.data() || {};
 
@@ -26,81 +19,75 @@ export const updateClub = functions.firestore
 
         // Read the summary doc
         return db.doc('clubs/summary').get()
-        .then(summaryDoc => {
-            const summary = summaryDoc.data() || {}
-            
-            summary.clubs[clubId] = club.name
+            .then(summaryDoc => {
+                const summary = summaryDoc.data() || {}
 
-            // Removing the club from summary
-            for (let i=0; i<7; i+=1) {
-                const day = summary[days[i]] || [];
-                for (let j=0; j<day.length; j+=1) {
-                    if ( day[j].clubId == clubId ) {
-                        //console.log('Removed from ', days[i])
-                        day.splice(j, 1)
-                        j-=1
-                    } 
+                summary.clubs[clubId] = club.name
+
+                // Removing the club from summary
+                for (let i = 0; i < 7; i += 1) {
+                    const day = summary[days[i]] || [];
+                    for (let j = 0; j < day.length; j += 1) {
+                        if (day[j].clubId == clubId) {
+                            //console.log('Removed from ', days[i])
+                            day.splice(j, 1)
+                            j -= 1
+                        }
+                    }
+                    summary[days[i]] = day
                 }
-                summary[days[i]] = day
-            }
-            // Adding the club to summary
-            for (let i = 0; i < 7; i += 1) {
-                const day : any = club[days[i]] || null;
-                if (!day) continue
-                summary[days[i]].push({
-                    clubId: clubId,
-                    name: club.name || 'No Name',
-                    location: day.location || '',
-                    start: day.start || '',
-                    end: day.end || ''
-                })
-            }
+                // Adding the club to summary
+                for (let i = 0; i < 7; i += 1) {
+                    const day: any = club[days[i]] || null;
+                    if (!day) continue
+                    summary[days[i]].push({
+                        clubId: clubId,
+                        name: club.name || 'No Name',
+                        location: day.location || '',
+                        start: day.start || '',
+                        end: day.end || ''
+                    })
+                }
 
-            return db.doc('clubs/summary').set(summary);
-        })
+                // Irregular Clubs
+                if (summary.irregulars == null) summary.irregulars = []
 
-        // Get an object representing the document
-        // e.g. {'name': 'Marie', 'age': 66}
-        //const newValue = change.after.data() || {};
+                // Remove club from irregulars
+                for (let i = 0; i < summary.irregulars.length; i += 1)
+                    if (summary.irregulars[i].clubId == clubId) {
+                        summary.irregulars.splice(i, 1); i -= 1;
+                    }
 
-        // ...or the previous value before this update
-        //const previousValue = change.before.data();
+                // Add club to irregulars
+                if (club.irregular)
+                    summary.irregulars.push({
+                        clubId: clubId,
+                        name: club.name || 'No Name'
+                    })
 
-        // access a particular field as you would any JS property
-        //const name = newValue.name;
-
-        // perform desired operations ...
-        
-        /*let count: number = (newValue.count || 0)
-        if (count % 2 == 0) return 'not changed'
-
-        return change.after.ref.set({
-            name_change_count: count + 1
-        }, { merge: true }).then(() => {
-            console.log("man, that's cool")
-            return "yep"
-        } );*/
+                return db.doc('clubs/summary').set(summary);
+            })
     });
 
 export const requestAttendance = functions.https.onCall((data, context) => {
     if (context.auth!.uid) {
         return db.collection("clubs")
-          .doc(data.club)
-          .collection("days")
-          .doc(data.day)
-          .set(
-            {
-              requests: admin.firestore.FieldValue.arrayUnion(
-                context.auth!.token.email
-              )
-            },
-            { merge: true }
-          )
-          .then( () => 'Request sent successfully!')
-          .catch( (err) => {
-              console.error(err)
-              return 'Error sending request'
-          });
+            .doc(data.club)
+            .collection("days")
+            .doc(data.day)
+            .set(
+                {
+                    requests: admin.firestore.FieldValue.arrayUnion(
+                        context.auth!.token.email
+                    )
+                },
+                { merge: true }
+            )
+            .then(() => 'Request sent successfully!')
+            .catch((err) => {
+                console.error(err)
+                return 'Error sending request'
+            });
     } else {
         return 'Permission denied: Logged in users only'
     }
